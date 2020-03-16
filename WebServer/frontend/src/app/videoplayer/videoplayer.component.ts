bb6a4974f4ccbc6c8ba4ab9fd9ad52395f5421bf
Anne Ejsing <anneejsing77@gmail.com>
2020-03-16 09:55:59 +0100

    Integrate stream into angular

--- WebServer/frontend/src/app/videoplayer/videoplayer.component.ts
+++ WebServer/frontend/src/app/videoplayer/videoplayer.component.ts
@@ -1,12 +1,4 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { VgHLS, VgDASH, IDRMLicenseServer, BitrateOption, VgAPI } from 'ngx-videogular';
-import { Subscription, timer } from 'rxjs';
-
-export interface IMediaStream {
-  type: 'vod' | 'hls';
-  source: string;
-  label: string;
-}
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-videoplayer',
@@ -15,55 +7,9 @@ export interface IMediaStream {
 })
 
 export class VideoplayerComponent implements OnInit {
-  @ViewChild(VgDASH) vgDash: VgDASH;
-  @ViewChild(VgHLS) vgHls: VgHLS;
-
-  currentStream: IMediaStream;
-  api: VgAPI;
-
-  bitrates: BitrateOption[];
-
-  streams: IMediaStream[] = [
-      {
-          type: 'vod',
-          label: 'VOD',
-          source: 'http://static.videogular.com/assets/videos/videogular.mp4'
-      },
-      {
-          type: 'hls',
-          label: 'HLS: Streaming',
-          source: 'https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8'
-      }
-  ];
-
   constructor() {
   }
 
-  onPlayerReady(api: VgAPI) {
-      this.api = api;
-  }
-
   ngOnInit() {
-      this.currentStream = this.streams[0];
-  }
-
-  setBitrate(option: BitrateOption) {
-    if(this.currentStream.type == 'hls')
-      this.vgHls.setBitrate(option);
-  }
-
-  onClickStream(stream: IMediaStream) {
-      this.api.pause();
-      this.bitrates = null;
-
-      let t: Subscription = timer(0, 10).subscribe(
-          () => {
-              this.currentStream = stream;
-              t.unsubscribe();
-          }
-      );
   }
 }
-
-
-//https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8
