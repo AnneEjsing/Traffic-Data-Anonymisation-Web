c442d482c1ba0e9d6db4c01385bc123dd643e733
Lars <lrecht16@student.aau.dk>
2020-03-25 12:50:35 +0100

    Sets up the dbresolver with the database through docker-compose * adds a database call to login

--- WebServer/microservices/db/dbresolver.py
+++ WebServer/microservices/db/dbresolver.py
@@ -1,18 +1,17 @@
-
-# import the connect library from psycopg2
+import argparse
 from psycopg2 import connect
-
-#Exampe query: f"SELECT * FROM {table_name};"
+from flask import Response, Flask, request
 
 # Sends a query to the database and returns the response.
 # Inspired by: #https://kb.objectrocket.com/postgresql/python-and-postgresql-docker-container-part-2-1063
-def executeQuery(query):
+def executeQuery(query,*inputs):
     # declare connection instance
     conn = connect(
-        dbname = "Traffic_Annonymisation_DB",
+        dbname = "traffic_annonymisation_db",
         user = "postgres",
-        host = "172.28.1.4",
-        password = "postgres"
+        host = "postgres",
+        password = "postgres",
+        port = 5432
     )
 
     results = []
@@ -21,8 +20,9 @@ def executeQuery(query):
         # declare a cursor object from the connection
         cursor = conn.cursor()
 
-        # execute an SQL statement using the psycopg2 cursor object
-        cursor.execute(query)
+        # execute an SQL statement using the psycopg2 cursor object.
+        # By seperating the query and the inputs, psycopg2 does sanitation.
+        cursor.execute(query,inputs)
 
         # enumerate() over the PostgreSQL records
         for i, record in enumerate(cursor):
@@ -39,3 +39,32 @@ def executeQuery(query):
         conn.close()
 
     return results
+
+def sanitise(input):
+    return input
+
+#Create the web app
+app = Flask(__name__)
+
+@app.route('/login',methods=['POST'])
+def login():
+    username = request.form['username']    
+    password = request.form['password']
+    query = """
+    SELECT * 
+    FROM users 
+    WHERE users.email = %s
+        AND users.password = md5(CONCAT(%s,users.salt));
+    """
+    if len(executeQuery(query,username,password)) == 1:
+        return Response("success",200)
+    else:
+        return Response("Not success", 401)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Provides different function calls to retrieve data from the database.'
+    )
+
+    args = parser.parse_args()
+    app.run(host='0.0.0.0',port=1337,debug=True)
