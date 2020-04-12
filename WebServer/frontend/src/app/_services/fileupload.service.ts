d1719df5249fd14df127473331a30528f06b2734
Anne Ejsing <anneejsing77@gmail.com>
2020-04-12 14:07:07 +0200

    Add service to fileuploads

--- WebServer/frontend/src/app/_services/fileupload.service.ts
+++ WebServer/frontend/src/app/_services/fileupload.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import * as global from "./disptacher-connection.service"
+import * as global from "./dispatcherConnection.service";
 
 @Injectable({
   providedIn: "root"
@@ -10,25 +10,30 @@ export class FileuploadService {
   readonly dispatcherUrl = global.dispatcherUrl;
   constructor(private http: HttpClient) {}
 
-  async postFile(file: File, cameraUrl: string) {
-    console.log(file)
+  async postFile(file: File, cameraIp: string) {
     let formData:FormData = new FormData();
     formData.append('file', file, file.name);
-    formData.append('url', cameraUrl);
-    formData.forEach((value,key) => {
-      console.log(key+" "+value)
-    });
+    formData.append('ip', cameraIp);
     
     var endpoint: string = this.dispatcherUrl + "model/upload"
-
-    let res = await this.http
-      .post(endpoint, formData, { headers: new HttpHeaders(), responseType: "text" })
-      .toPromise()
-      .then(
-        data => { return '200'; },
-        error => { return error.status; }
-      );
-
+    let res = await this.http.post(
+      endpoint,
+      formData,
+      this.constructHttpOptions())
+      .toPromise().then(
+        data => { return '200' },
+        error => { return error.status }
+      )
     return res;
+  }
+
+  constructHttpOptions() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + localStorage.getItem('session_token'),
+      })
+    };
+
+    return httpOptions;
   }
 }
