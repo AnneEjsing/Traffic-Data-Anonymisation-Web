8a6a117926f668e04e3bf29288a94b8f1fcf3422
Anne Ejsing <anneejsing77@gmail.com>
2020-05-01 20:05:41 +0200

    100 % code coverage in camera

--- WebServer/microservices/db/unittest/camera_test.py
+++ WebServer/microservices/db/unittest/camera_test.py
@@ -8,6 +8,7 @@ import psycopg2
 import testing.postgresql
 import aiounittest
 
+# mock request to resemble aiohttp request
 class request:
     dic = {}
     def __init__(self,dict):
@@ -28,8 +29,7 @@ def initailise_database(postgresql):
     os.environ["POSTGRES_PASSWORD"] = ""
     os.environ["POSTGRES_PORT"] = str(postgresql.dsn()['port'])
 
-
-class CamearaGetCreateTests(aiounittest.AsyncTestCase):
+class CamearaGetTests(aiounittest.AsyncTestCase):
     @classmethod
     def setUpClass(cls):
         cls.postgresql = testing.postgresql.Postgresql(port=7654)
@@ -55,7 +55,7 @@ class CamearaGetCreateTests(aiounittest.AsyncTestCase):
     async def test_camera_get_fail_input_is_not_uuid(self):
         req = request({"id":"b0eebc99"})
         response = await camera.camera_get(req)
-        self.assertEqual(response.status, 404)
+        self.assertEqual(response.status, 500)
 
     async def test_camera_get_fail_missing_input(self):
         req = request({})
@@ -79,13 +79,108 @@ class CamearaGetCreateTests(aiounittest.AsyncTestCase):
     async def test_camera_get_all_for_user_fail_input_is_not_uuid(self):
         req = request({"id":7})
         response = await camera.camera_userlist(req)
-        print(response.body)
         self.assertEqual(response.status, 500)    
 
     async def test_camera_get_all_for_user_fail_missing_input(self):
         req = request({})
         response = await camera.camera_userlist(req)
-        self.assertEqual(response.status, 500)   
-    
+        self.assertEqual(response.status, 500)
+
+class CamearaCreateTests(aiounittest.AsyncTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.postgresql = testing.postgresql.Postgresql(port=7654)
+        initailise_database(cls.postgresql)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.postgresql.stop()
+    ## CREATE CAMERA   
+    async def test_camera_create_camera_pass(self):
+        req = request({'owner':'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380b12', 'description':'XXX', 'ip':'XXX', 'label':'XXX', 'source':'XXX'})
+        response = await camera.camera_create(req)
+        self.assertEqual(response.status, 200)
+
+    async def test_camera_create_camera_fail_missing_input(self):
+        req = request({'description':'XXX', 'ip':'XXX', 'label':'XXX', 'source':'XXX'})
+        response = await camera.camera_create(req)
+        self.assertEqual(response.status, 500)
+
+    async def test_camera_create_camera_fail_non_existing_owner(self):
+        req = request({'owner':'00000000-9c0b-4ef8-bb6d-6bb9bd380b12','description':'XXX', 'ip':'XXX', 'label':'XXX', 'source':'XXX'})
+        response = await camera.camera_create(req)
+        self.assertEqual(response.status, 500)
+
+class CamearaUpdateTests(aiounittest.AsyncTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.postgresql = testing.postgresql.Postgresql(port=7654)
+        initailise_database(cls.postgresql)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.postgresql.stop()
+
+    ## UPDATE INFO
+    async def test_camera_update_info_pass(self):
+        req = request({"camera_id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11", "description": "test", "label": "test", "ip": "0.0.0.0", "source": "https://test.m3u8", "last_sign_of_life": "null", "owner": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380b12"})
+        res = b'{"camera_id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11", "description": "test", "label": "test", "ip": "0.0.0.0", "source": "https://test.m3u8", "last_sign_of_life": null, "owner": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380b12"}'
+        response = await camera.camera_update(req)
+        self.assertEqual(response.body, res)
+
+    async def test_camera_update_info_fail_wrong_id(self):
+        req = request({"camera_id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12", "description": "This is a description for the open cam", "label": "open cam", "ip": "0.0.0.0", "source": "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8", "last_sign_of_life": "null", "owner": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380b12"})
+        response = await camera.camera_update(req)
+        self.assertEqual(response.status, 500)
+
+    async def test_camera_update_info_fail_missing_input(self):
+        req = request({"description": "This is a description for the open cam", "label": "open cam", "ip": "0.0.0.0", "source": "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8", "last_sign_of_life": "null", "owner": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380b12"})
+        response = await camera.camera_update(req)
+        self.assertEqual(response.status, 500)
+
+    ## UPDATE LAST SIGN OF LIFE
+    async def test_camera_update_lsol_pass(self):
+        req = request({"id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"})
+        response = await camera.camera_updatelsol(req)
+        self.assertEqual(response.status, 200)
+
+    async def test_camera_update_lsol_fail_wrong_id(self):
+        req = request({"camera_id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12"})
+        response = await camera.camera_updatelsol(req)
+        self.assertEqual(response.status, 500)
+
+    async def test_camera_update_lsol_fail_missing_input(self):
+        req = request({})
+        response = await camera.camera_updatelsol(req)
+        self.assertEqual(response.status, 500)
+
+class CamearaDeleteTests(aiounittest.AsyncTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.postgresql = testing.postgresql.Postgresql(port=7654)
+        initailise_database(cls.postgresql)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.postgresql.stop()
+
+    async def test_camera_delete_pass(self):
+        req = request({})
+        req.query={"id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"}
+        response = await camera.camera_delete(req)
+        self.assertEqual(response.status, 200)
+
+    async def test_camera_delete_fail_invalid_uuid(self):
+        req = request({})
+        req.query={"id": "00000000-9c0b-4ef8-bb6d-6bb9bd380a11"}
+        response = await camera.camera_delete(req)
+        self.assertEqual(response.status, 404)
+
+    async def test_camera_delete_fail_missing_input(self):
+        req = request({})
+        req.query={}
+        response = await camera.camera_delete(req)
+        self.assertEqual(response.status, 500)
+
 if __name__ == '__main__':
     unittest2.main()
