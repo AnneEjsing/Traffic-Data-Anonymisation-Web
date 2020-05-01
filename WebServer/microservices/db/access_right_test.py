571e8bde45db99219c55ad9687fac87c9ec02b06
Anne Ejsing <anneejsing77@gmail.com>
2020-05-01 09:57:00 +0200

    Make setup and teardown for class and not method

--- WebServer/microservices/db/access_right_test.py
+++ WebServer/microservices/db/access_right_test.py
@@ -19,41 +19,38 @@ class request:
         return self.dic
 
 class MyTest(aiounittest.AsyncTestCase):
-    def setUp(self):
-        self.postgresql = testing.postgresql.Postgresql(port=7654)
-        with psycopg2.connect(**self.postgresql.dsn()) as conn:
+    @classmethod
+    def setUpClass(cls):
+        cls.postgresql = testing.postgresql.Postgresql(port=7654)
+        with psycopg2.connect(**cls.postgresql.dsn()) as conn:
             with conn.cursor() as cursor:
                 with open("test_data.sql","r") as f:
                     cursor.execute(f.read())
             conn.commit()
             
-        os.environ['POSTGRES_DB'] = self.postgresql.dsn()['database']
-        os.environ['POSTGRES_USER'] = self.postgresql.dsn()['user']
-        os.environ['POSTGRES_HOST'] = self.postgresql.dsn()['host']
+        os.environ['POSTGRES_DB'] = cls.postgresql.dsn()['database']
+        os.environ['POSTGRES_USER'] = cls.postgresql.dsn()['user']
+        os.environ['POSTGRES_HOST'] = cls.postgresql.dsn()['host']
         os.environ["POSTGRES_PASSWORD"] = ""
-        os.environ["POSTGRES_PORT"] = str(self.postgresql.dsn()['port'])
+        os.environ["POSTGRES_PORT"] = str(cls.postgresql.dsn()['port'])
 
-    
-    def tearDown(self):
-        self.postgresql.stop()
+    @classmethod
+    def tearDownClass(cls):
+        cls.postgresql.stop()
 
     @asyncio.coroutine
-    async def test_get(self):
-        dbresolver.connection_func = lambda: psycopg2.connect(**self.postgresql.dsn())
+    async def test_get(cls):
+        dbresolver.connection_func = lambda: psycopg2.connect(**cls.postgresql.dsn())
         req = request({"camera_id":"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11", "user_id":"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380b11"})
-
         response = await access_right.right_get(req)
-
-        self.assertEqual(response.body, b'[{"camera_id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11", "user_id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380b11"}]')
+        cls.assertEqual(response.body, b'[{"camera_id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11", "user_id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380b11"}]')
 
     @asyncio.coroutine
-    async def test_get_wrong_uid(self):
-        dbresolver.connection_func = lambda: psycopg2.connect(**self.postgresql.dsn())
+    async def test_get_wrong_uid(cls):
+        dbresolver.connection_func = lambda: psycopg2.connect(**cls.postgresql.dsn())
         req = request({"camera_id":"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11", "user_id":"b0eebc99-9c0b-4ef8-bb6d-6bb9bd380b11"})
-        
         response = await access_right.right_get(req)
-
-        self.assertEqual(response.body, b'[]')
+        cls.assertEqual(response.body, b'[]')
     
 
 if __name__ == '__main__':
