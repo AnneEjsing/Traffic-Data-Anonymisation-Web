0e4968f632e55cdce9f78363f07072ccb7012a38
Lars <lrecht16@student.aau.dk>
2020-05-07 10:00:03 +0200

    The list videos is back to save the video deleter

--- WebServer/microservices/db/video.py
+++ WebServer/microservices/db/video.py
@@ -80,3 +80,10 @@ async def video_create(request):
     result, error = dbresolver.execute_query(query, user_id, camera_id, video_thumbnail)
     if error: return web.Response(text=str(error),status=500)
     return dbresolver.has_one_result(result, "Something went wrong", 500)
+
+@routes.get('/video/list')
+def video_list(request):
+    query = "SELECT * FROM recorded_videos;"
+    result, error = executeQuery(query)
+    if error: return web.Response(text=str(error),status=500)
+    return web.Response(text=json.dumps(result, default=str),status=200)
