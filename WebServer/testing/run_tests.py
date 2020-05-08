ccfe95dcb06c163d895feaac2fed64f435f6e45c
Lars <lrecht16@student.aau.dk>
2020-05-08 11:57:42 +0200

    Adds script to run all unit tests

---
+++ WebServer/testing/run_tests.py
@@ -0,0 +1,23 @@
+import os
+import subprocess as sp
+import unittest
+
+
+
+def run_unit_tests():
+    os.chdir(os.getcwd() + "/../microservices/db/unittest")
+    sp.call("python -m unittest discover -s . -p '*_test.py'",shell=True)
+    os.chdir(os.getcwd() + "/../../dispatcher/unittest")
+    sp.call("python -m unittest discover -s . -p '*_test.py'",shell=True)
+    os.chdir(os.getcwd() + "/../../video-deleter/unittest")
+    sp.call("python -m unittest discover -s . -p '*_test.py'",shell=True)
+    os.chdir(os.getcwd() + "/../../video-download/unittest")
+    sp.call("python -m unittest discover -s . -p '*_test.py'",shell=True)
+    return True
+
+
+
+if __name__ == "__main__":
+    run_unit_tests()
+
+
