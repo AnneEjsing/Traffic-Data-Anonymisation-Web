74cf20a3f8e7d96695068160cada5cf0a2770663
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-05-11 11:21:14 +0200

    Runs all tests with python3 now

--- WebServer/testing/run_tests.py
+++ WebServer/testing/run_tests.py
@@ -6,13 +6,13 @@ import unittest
 
 def run_unit_tests():
     os.chdir(os.getcwd() + "/../microservices/db/unittest")
-    sp.call("python -m unittest discover -s . -p '*_test.py'",shell=True)
+    sp.call("python3 -m unittest discover -s . -p '*_test.py'",shell=True)
     os.chdir(os.getcwd() + "/../../dispatcher/unittest")
-    sp.call("python -m unittest discover -s . -p '*_test.py'",shell=True)
+    sp.call("python3 -m unittest discover -s . -p '*_test.py'",shell=True)
     os.chdir(os.getcwd() + "/../../video-deleter/unittest")
-    sp.call("python -m unittest discover -s . -p '*_test.py'",shell=True)
+    sp.call("python3 -m unittest discover -s . -p '*_test.py'",shell=True)
     os.chdir(os.getcwd() + "/../../video-download/unittest")
-    sp.call("python -m unittest discover -s . -p '*_test.py'",shell=True)
+    sp.call("python3 -m unittest discover -s . -p '*_test.py'",shell=True)
     return True
 
 
