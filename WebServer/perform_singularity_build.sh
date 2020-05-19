b3de9ac21a42b22a09aabc564f1e0000dd290d50
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-05-19 09:07:55 +0200

    Fix singularity build script

--- WebServer/perform_singularity_build.sh
+++ WebServer/perform_singularity_build.sh
@@ -3,102 +3,112 @@
 WD=$(pwd)
 
 camera(){
+    echo ""
     echo "Building camera service..."
     cd microservices/camera-service
     singularity build -F ../../singularity_containers/camera-service/camera-service.sif camera.def
-    cd $WD
-    echo ""
 }
 
 dbresolver(){
+    echo ""
     echo "Building dbresolver..."
     cd microservices/db
     singularity build -F ../../singularity_containers/dbresolver/dbresolver.sif dbresolver.def
-    cd $WD
-    echo ""
 }
 
 database(){
+    echo ""
     echo "Building database..."
     cd microservices/db
     singularity build -F ../../singularity_containers/postgres/db.sif postgres.def
-    cd $WD
-    echo ""
 }
 
 dispatcher(){
+    echo ""
     echo "Building dispatcher..."
     cd microservices/dispatcher
     singularity build -F ../../singularity_containers/dispatcher/dispatcher.sif dispatcher.def
-    cd $WD
-    echo ""
 }
 
 model-changer(){
+    echo ""
     echo "Building model-changer..."
     cd microservices/model-changer
     singularity build -F ../../singularity_containers/model-changer/model-changer.sif model-changer.def
-    cd $WD
-    echo ""
 }
 
 profile(){
+    echo ""
     echo "Building profile-service..."
     cd microservices/profile-service
     singularity build -F ../../singularity_containers/profile-service/profile-service.sif profile-service.def
-    cd $WD
-    echo ""
 }
 
 streamer(){
+    echo ""
     echo "Building streamer..."
     cd microservices/streamer
     singularity build -F ../../singularity_containers/streamer/streamer.sif streamer.def
-    cd $WD
-    echo ""
 }
 
 video-deleter(){
+    echo ""
     echo "Building video-deleter..."
     cd microservices/video-deleter
-    singularity build -F ../../singularity_containers/video-deleter/video-deleter.sif video-deleter.sif
-    cd $WD
-    echo ""
+    singularity build -F ../../singularity_containers/video-deleter/video-deleter.sif video-deleter.def
 }
 
 video-saver(){
-    echo "Building video-file-saver..."
-    cd microservices/video-downloader
-    singularity build -F ../../singularity_containers/video-file-saver/video-file-saver.sif video-file-saver.def
-    cd $WD
     echo ""
+    echo "Building video-file-saver..."
+    cd microservices/video-download
+    singularity build -F ../../singularity_containers/video-file-saver/video-file-saver.sif video-file-saver.def
 }
 
 video-service(){
+    echo ""
     echo "Building video-service..."
     cd microservices/video-service
     singularity build -F ../../singularity_containers/video-service/video-service.sif video-service.def
-    cd $WD
-    echo ""
 }
 
 frontend(){
+    echo ""
     echo "Building frontend..."
     cd frontend
     singularity build -F ../singularity_containers/frontend/frontend.sif frontend.def
-    cd $WD
-    echo ""
 }
 
 
 camera && \
+cd $WD && \
+
 dbresolver && \
+cd $WD && \
+
 database && \
+cd $WD && \
+
 dispatcher &&  \
+cd $WD && \
+
 model-changer && \
+cd $WD && \
+
 profile && \
+cd $WD && \
+
 streamer && \
+cd $WD && \
+
 video-deleter && \
+cd $WD && \
+
 video-saver && \
+cd $WD && \
+
 video-service && \
-frontend
+cd $WD && \
+
+frontend && \
+cd $WD
